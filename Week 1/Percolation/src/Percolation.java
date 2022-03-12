bc6b72d2418ab02ec45e3cf51c4ebd75003a8548
Omar Shawky <ipowerfuliii@gmail.com>
2022-03-12 23:30:36 +0200

    Before creating randomOpen function to open blocked sites with probability 0.6

--- Week 1/Percolation/src/Percolation.java
+++ Week 1/Percolation/src/Percolation.java
@@ -1,5 +1,3 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -8,23 +6,25 @@ public class Percolation {
     private boolean site[][]; //2D array containing cells
     private int size; //n variable that is passed to constructor, assigned only once at
     // instantiation.
-    private int openSites; //number of open cells in the 2D array
+    private int openSitesNumber; //number of open cells in the 2D array
     private WeightedQuickUnionUF uf;
 
+    // TODO: Performance requirements. The constructor must take time proportional to n2;
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n<1) throw new IllegalArgumentException();
 
-        site = new boolean[n][n];
-        size = n;
-        openSites = 0;
-
-        // Instantiation of n^2 number of cells + 2 virtual upper & lower cells
+        // Instantiation of n^2 number of cells + 2 virtual upper & lower cells initially blocked.
         uf = new WeightedQuickUnionUF(n*n+2);
+//        site = new boolean[n][n]; // do not think it is necessary but kept in case it is
+
+        size = n;
+        openSitesNumber = 0;
+
     }
     /*
-    TODO!!
-     1. Performance requirements. The constructor must take time proportional to n2; all instance methods must take constant time plus a constant number of calls to union() and find().
+    TODO: all instance methods must take constant time plus a constant number of calls to union()
+      and find().
      */
     // opens the site (row, col) if it is not open already
     public void open (int row, int col) {
@@ -67,7 +67,7 @@ public class Percolation {
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return openSites;
+        return openSitesNumber;
     }
 
     // does the system percolate?
@@ -84,8 +84,35 @@ public class Percolation {
     }
 
     // test client (optional)
-
     public static void main(String args[]) {
-        System.out.print("Hello World");
+//        System.out.print("Hello World");
+
+        //Top-to-bottom approach
+
+        // New percolation for testing
+        Percolation percolation = new Percolation(3);
+
+        //TODO: Continue from here.
+
+        // Create random open sites in the board
+        //Problem: did not know how to use this probability to create a random open sites.
+        double prob = 0.6; // Use percolation probability from assignment itself
+
+        //Problem: cannot use Random library (prohibited in the assignment)
+        /*
+        for (int i=0; i<10; i++){
+
+        }
+        */
+
+        /*
+        //loop on percolation until it percolates
+        while (!percolation.percolates()){
+            //TODO: write the logic on how to open new site.
+
+
+
+        }
+         */
     }
 }
