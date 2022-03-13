e958870233313d0833c68181567f5f0a1b7fd28b
Omar Shawky <ipowerfuliii@gmail.com>
2022-03-13 14:37:29 +0200

    working after adding n++

--- Week 1/Percolation/src/Percolation.java
+++ Week 1/Percolation/src/Percolation.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -14,9 +16,11 @@ public class Percolation {
     public Percolation(int n) {
         if (n<1) throw new IllegalArgumentException();
 
+        n++;
+
         // Instantiation of n^2 number of cells + 2 virtual upper & lower cells initially blocked.
-        uf = new WeightedQuickUnionUF(n*n+2);
-//        site = new boolean[n][n]; // do not think it is necessary but kept in case it is
+        uf = new WeightedQuickUnionUF((n*n+2));
+        site = new boolean[n][n]; // do not think it is necessary but kept in case it is
 
         size = n;
         openSitesNumber = 0;
@@ -28,37 +32,33 @@ public class Percolation {
      */
     // opens the site (row, col) if it is not open already
     public void open (int row, int col) {
-        checkCellValidity (row, col);
+        if (!checkCellValidity (row, col)) throw new IllegalArgumentException();
 
-        //starting count from 0 instead of 1
-        row -= 1;
-        col -= 1;
-
-        site[row][col] = true; //Opening site
+        site[row-1][col-1] = true; //Opening site
 
         // connect sites that has been opened from left, right, up & down.
 
         //connect left
         //check first if we are at the far left, so we don't reach out of bound error
-        if (row!=0 && isOpen(row, col-1)){
+        if (checkCellValidity(row, col-1) && isOpen(row, col-1)){
             uf.union((row*size)+col, (row*size)+(col-1));
         }
 
         //connect right
         //check first if we are at the far right, so we don't reach out of bound error
-        if (row!=size && isOpen(row, col+1)){
+        if (checkCellValidity(row, col+1) && isOpen(row, col+1)){
             uf.union((row*size)+col, (row*size)+(col+1));
         }
 
         //connect up
         //check first if we are at the top, so we don't reach out of bound error
-        if (col!=0 && isOpen(row, col-1)){
+        if (checkCellValidity(row-1, col) && isOpen(row-1, col)){
             uf.union(((row-1)*size)+col, (row*size)+col);
         }
 
         //connect bottom
         //check first if we are at the bottom, so we don't reach out of bound error
-        if (col!=size && isOpen(row, col+1)) {
+        if (checkCellValidity(row+1, col) && isOpen(row+1, col)) {
             uf.union(((row+1)*size)+col, (row*size)+col);
         }
 
@@ -66,30 +66,18 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        checkCellValidity (row, col);
+            // check first if index is in bound, return false otherwise
+            return checkCellValidity (row, col) &&
+            //if it is in bound, check if it is true/false
+                    site[row - 1][col - 1]; //check if site is open
 
-        return site[row][col]; //check if site is open (return it)
     }
 
     // is the site (row, col) full?
     //i.e., is this site connected to the virtual upper or lower site?
     public boolean isFull(int row, int col) {
         checkCellValidity (row, col);
-
-        //starting count from 0 instead of 1
-        row -= 1;
-        col -= 1;
-
-        // check if the cell is open before we check it is connected to upper/lower
-        if (!isOpen(row, col)) {
-
-            //check if current point is connected to virtual uppper/lower
-
-            return false;
-        }
-        else {
-            return false;
-        }
+        return false;
     }
 
     // returns the number of open sites
@@ -97,21 +85,26 @@ public class Percolation {
         return openSitesNumber;
     }
 
-    // does the system percolate?
+    // does the system percolte?
     //i.e., is the virtual upper point connected to virtual bottom point?
-    public boolean percolates() {
+    public boolean percoltes() {
         return false;
     }
 
     // Determine if it is a valid cell (lies between 1 & n)
-    private void checkCellValidity (int row, int col) {
-        if (row < 1 || row > size || col < 1 || col > size) {
-            throw new java.lang.IllegalArgumentException();
-        }
+    private boolean checkCellValidity (int row, int col) {
+        return row >= 1 && row <= size && col >= 1 && col <= size;
     }
 
     // test client (optional)
     public static void main(String args[]) {
-//        System.out.print("Hello World");
+
+        //Instance
+        Percolation percolation = new Percolation(2);
+
+        percolation.open(1,1);
+        percolation.open(1,2);
+        percolation.open(2,1);
+        percolation.open(2,2);
     }
 }
