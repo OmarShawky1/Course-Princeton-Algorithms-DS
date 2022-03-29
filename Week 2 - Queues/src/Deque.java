91fff9cf21e70145597916c249e69636ae784bc6
Omar Shawky <ipowerfuliii@gmail.com>
2022-03-29 21:02:12 +0200

    Stopped at iterator()

--- Week 2 - Queues/src/Deque.java
+++ Week 2 - Queues/src/Deque.java
@@ -3,35 +3,60 @@ import java.util.LinkedList;
 
 public class Deque<Item> implements Iterable<Item> {
 
+    private LinkedList<Item> list; //TODO to be removed after i finish implementing
     //Global Variables
-    private LinkedList<Item> list;
+    private Node first = null;
+    private Node last = null;
 
+
+    //Private Class Node, It contains Next, Previous & Data (because it is double ended)
+    private class Node {
+        private Node next;
+        private Node previous;
+        private Item data;
+        public Node(Node next, Node previous, Item data) {
+            this.next = next;
+            this.previous = previous;
+            this.data = data;
+        }
+    }
     // construct an empty deque
     public Deque() {
     }
     // is the deque empty?
     public boolean isEmpty() {
-        return list.isEmpty();
+        return first == null;
     }
     // return the number of items on the deque
     public int size() {
-        return list.size();
+        int counter = 0;
+        while (first.next != null){
+            first = first.next;
+            counter++;
+        }
+        return counter;
     }
     // add the item to the front
     public void addFirst(Item item) {
-        list.addFirst(item);
+        first.previous = new Node(first, null, item);
+        first = first.previous;
     }
     // add the item to the back
     public void addLast(Item item) {
-        list.addLast(item);
+        last.next = new Node(null, last, item);
+        last = last.next;
     }
     // remove and return the item from the front
     public Item removeFirst() {
-        return list.removeFirst();
+        Item firstData = first.data;
+        first = first.next;
+        return firstData;
     }
     // remove and return the item from the back
     public Item removeLast() {
-        return list.removeLast();
+        Item lastData = last.data;
+        last = last.previous;
+        return lastData;
     }
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
