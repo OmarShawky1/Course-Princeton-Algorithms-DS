69c19e027a6a60e940d8a5976365a0f2e66c8d41
Omar Shawky <ipowerfuliii@gmail.com>
2022-03-30 21:17:46 +0200

    Implemented everything but didn't test at all

--- Week 2 - Queues/src/Deque.java
+++ Week 2 - Queues/src/Deque.java
@@ -1,13 +1,11 @@
 import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
-    private LinkedList<Item> list; //TODO to be removed after i finish implementing
     //Global Variables
     private Node first = null;
     private Node last = null;
-
 
     //Private Class Node, It contains Next, Previous & Data (because it is double ended)
     private class Node {
@@ -20,8 +18,36 @@ public class Deque<Item> implements Iterable<Item> {
             this.data = data;
         }
     }
+
+    private class ListIterator<Item> implements Iterator<Item> {
+
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current.next != null;
+        }
+
+        public Item next() {
+            checkListEmpty();
+            Item item = (Item) current.data;
+            current = current.next;
+            return item;
+        }
+
+        public void remove(){
+            throw new UnsupportedOperationException("remove() should not be called");
+        }
+    }
     // construct an empty deque
-    public Deque() {
+    public Deque(Node ...nodes) {
+        Node pointer = first;
+        for (Node node: nodes
+             ) {
+            pointer.next = node;
+            node.previous = pointer;
+            pointer = pointer.next;
+        }
+        last = pointer; //pointer will stop at the last node that has been added
     }
     // is the deque empty?
     public boolean isEmpty() {
@@ -38,32 +64,51 @@ public class Deque<Item> implements Iterable<Item> {
     }
     // add the item to the front
     public void addFirst(Item item) {
+        itemValid(item);
         first.previous = new Node(first, null, item);
         first = first.previous;
     }
     // add the item to the back
     public void addLast(Item item) {
+        itemValid(item);
         last.next = new Node(null, last, item);
         last = last.next;
     }
     // remove and return the item from the front
     public Item removeFirst() {
+        checkListEmpty();
         Item firstData = first.data;
         first = first.next;
         return firstData;
     }
     // remove and return the item from the back
     public Item removeLast() {
+        checkListEmpty();
         Item lastData = last.data;
         last = last.previous;
         return lastData;
     }
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
-        return list.iterator();
+        return new ListIterator<>();
     }
+
+    // Check if the item is null
+    private void itemValid(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("I was called with to add/remove null");
+        }
+    }
+
+    // Check if list, being operated, is empty
+    private void checkListEmpty() {
+        if (first == null) {
+            throw new NoSuchElementException("I was called to remove first/last on an empty list");
+        }
+    }
+
     // unit testing (required)
     public static void main(String[] args) {
-
+        
     }
 }
