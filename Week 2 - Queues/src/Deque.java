127a859238554728de3b6d204f7a202e43676bb0
Omar Shawky <ipowerfuliii@gmail.com>
2022-04-02 13:01:07 +0200

    Corrected constructor bug, added toString() method to print the list, added 2 test cases

--- Week 2 - Queues/src/Deque.java
+++ Week 2 - Queues/src/Deque.java
@@ -1,54 +1,26 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
     //Global Variables
-    private Node first = null;
-    private Node last = null;
+    private Node first;
+    private Node last;
 
-    //Private Class Node, It contains Next, Previous & Data (because it is double ended)
-    private class Node {
-        private Node next;
-        private Node previous;
-        private Item item;
-        public Node(Node next, Node previous, Item item) {
-            this.next = next;
-            this.previous = previous;
-            this.item = item;
+    // construct deque with initial values
+    public Deque(Item ...items) {
+
+        // Initialize first & last pointers to null nodes
+        first = new Node(null, null, null);
+        last = new Node(null, null, null);
+
+        //Add Initial values
+        for (Item item : items) {
+            addFirst(item);
         }
     }
 
-    private class ListIterator<Item> implements Iterator<Item> {
-
-        private Node current = first;
-
-        public boolean hasNext() {
-            return current.next != null;
-        }
-
-        public Item next() {
-            checkListEmpty();
-            Item item = (Item) current.item;
-            current = current.next;
-            return item;
-        }
-
-        public void remove(){
-            throw new UnsupportedOperationException("remove() should not be called");
-        }
-    }
-    // construct an empty deque
-    public Deque(Node ...nodes) {
-        Node pointer = first;
-        for (Node node: nodes
-             ) {
-            pointer.next = node;
-            node.previous = pointer;
-            pointer = pointer.next;
-        }
-        last = pointer; //pointer will stop at the last node that has been added
-    }
     // is the deque empty?
     public boolean isEmpty() {
         return first == null;
@@ -65,12 +37,17 @@ public class Deque<Item> implements Iterable<Item> {
     // add the item to the front
     public void addFirst(Item item) {
         itemValid(item);
+
+        // Initialize a node that has nothing before it (null) and old first after it.
         Node newNode  = new Node(first, null, item);
 
+        // If list contains items, make current first.previous point to newNode & make it first
         if (!isEmpty()){
             first.previous = newNode;
             first = first.previous;
-        } else {
+        }
+        //If list is empty, then first & last nodes should point to the new node
+        else {
             first = newNode;
             last = newNode;
         }
@@ -132,8 +109,76 @@ public class Deque<Item> implements Iterable<Item> {
         }
     }
 
+    public String toString() {
+        StringBuilder listToString = new StringBuilder("[");
+        Iterator<Item> iterator = iterator();
+        boolean hasNext = iterator.hasNext();
+        while (hasNext){
+            listToString.append(iterator.next());
+            hasNext = iterator.hasNext();
+            if (hasNext) {
+                listToString.append(", ");
+            }
+        }
+        listToString.append("]");
+        return listToString.toString();
+    }
+
+    //Private Class Node, It contains Next, Previous & Data (because it is double ended)
+    private class Node {
+        private Node next;
+        private Node previous;
+        private Item item;
+        public Node(Node next, Node previous, Item item) {
+            this.next = next;
+            this.previous = previous;
+            this.item = item;
+        }
+    }
+
+    private class ListIterator<Item> implements Iterator<Item> {
+
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current.next != null;
+        }
+
+        public Item next() {
+            checkListEmpty();
+            Item item = (Item) current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove(){
+            throw new UnsupportedOperationException("remove() should not be called");
+        }
+    }
+
     // unit testing (required)
     public static void main(String[] args) {
+
+        System.out.println("###############Dequeue Tests###############");
+
+        System.out.println("##########My Own Test Cases##########");
+        System.out.println("####Test 1####");
+        System.out.println("Initialize an empty Double ended LinkedList");
+        Deque<String> temp = new Deque<>();
+        System.out.println("List: " +  temp);
+        System.out.println("####Test 1 End####");
+
+        System.out.println();
+
+        System.out.println("####Test 2####");
+        System.out.println("Initialize Double ended LinkedList with initial values");
+        Deque<String> deque = new Deque<>("1", "2", "3");
+        System.out.println("List after initialization: " +  deque);
+        System.out.println("hasNext test: " + deque);
+        System.out.println("####Test 2 End####");
+
+        System.out.println("##########End of my Tests##########");
+        System.out.println();
 
     }
 }
