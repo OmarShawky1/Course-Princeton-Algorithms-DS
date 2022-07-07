4319193aa8fdf150bde4e7597ff4364f8a79bf50
Omar Shawky <ipowerfuliii@gmail.com>
2022-07-07 20:14:56 +0200

    Re-implemented RandmoizedQueue using arrays

--- Week 2 - Queues/src/RandomizedQueue.java
+++ Week 2 - Queues/src/RandomizedQueue.java
@@ -1,56 +1,88 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
     //Global Variables
-    Deque<Item> deque;
+    Object[] queue;
+    int N;
+    private int first;
+
 
     // construct an empty randomized queue
-    public RandomizedQueue() {
-        deque = new Deque<>();
+    public RandomizedQueue(Item... items) {
+
+        int itemsLength = items.length;
+        queue = new Object[itemsLength];
+        first = 0;
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return deque.isEmpty();
+        return N==0;
     }
 
     // return the number of items on the randomized queue
-    public int size() {
-        return deque.size();
+    public int size(){
+        return N+1;
     }
 
     // add the item
     public void enqueue(Item item) {
-        deque.addLast(item);
+        queue[++N] = item;
     }
 
     // remove and return a random item
     public Item dequeue() {
-        //TODO
+        Item item = (Item) queue[first];
+        queue[first] = null;
+        first++;
+        return item;
     }
-
     // return a random item (but do not remove it)
     public Item sample() {
-
-        int randomIndex = StdRandom.uniform(deque.size()) +1;
-        Iterator<Item> iterator = deque.iterator();
-        Item randomItem = null;
-        for (int i = 0; i< randomIndex; i++) {
-            randomItem = iterator.next();
-        }
-        return randomItem;
+        return (Item) queue[first+ StdRandom.uniform(N) + 1];
     }
-
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-
+        return new ListIterator();
     }
 
     // unit testing (required)
-    public static void main(String[] args) {
+    private void checkListEmpty() {
+       if (queue[first] == null) {
+          throw new NoSuchElementException("I was called to remove first/last on an empty list");
+       }
+    }
 
+   private class ListIterator implements Iterator<Item> {
+
+      private int current = first;
+
+      public boolean hasNext() {
+         return queue[current] != null;
+      }
+
+      public Item next() {
+         checkListEmpty();
+         Item item = (Item) queue[current];
+         current++;
+         return item;
+      }
+
+      public void remove() {
+         throw new UnsupportedOperationException("remove() should not be called");
+      }
+   }
+
+    public static void main(String[] args) {
+       StdOut.println("###############RandomizedQueue Tests###############");
+
+       StdOut.println("##########My Own Test Cases##########");
+
+       StdOut.println("####Test 1####");
     }
 }
