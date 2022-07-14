75cedf517a9ca599b3960b62b4b6f201863f3a17
Omar Shawky <ipowerfuliii@gmail.com>
2022-07-14 22:16:01 +0200

    Finished Implementation and ready for testing

--- Week 2 - Queues/src/RandomizedQueue.java
+++ Week 2 - Queues/src/RandomizedQueue.java
@@ -27,38 +27,47 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // return the number of items on the randomized queue
     public int size(){
-        return N+1;
+        return N + 1;
     }
 
     // add the item
     public void enqueue(Item item) {
+        itemValid(item);
         queue[++N] = item;
+        adjustArraySize();
     }
 
     // remove and return a random item
     public Item dequeue() {
-        Item item = (Item) queue[first];
-        queue[first] = null;
-        first++;
+        checkArrayEmpty();
+        Item item;
+        int randomNumber;
+        do {
+            randomNumber = first + StdRandom.uniform(N) + 1;
+            item = (Item) queue[randomNumber];
+        } while (item != null);
+        queue[randomNumber] = null;
+        N--;
+        adjustArraySize();
         return item;
     }
     // return a random item (but do not remove it)
     public Item sample() {
-        return (Item) queue[first+ StdRandom.uniform(N) + 1];
+        checkArrayEmpty();
+        Item item;
+        int randomNumber;
+        do {
+            randomNumber = first + StdRandom.uniform(N) + 1;
+            item = (Item) queue[randomNumber];
+        } while (item != null);
+        return item;
     }
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
         return new ListIterator();
     }
 
-    // unit testing (required)
-    private void checkListEmpty() {
-       if (queue[first] == null) {
-          throw new NoSuchElementException("I was called to remove first/last on an empty list");
-       }
-    }
-
-   private class ListIterator implements Iterator<Item> {
+    private class ListIterator implements Iterator<Item> {
 
       private int current = first;
 
@@ -67,7 +76,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
       }
 
       public Item next() {
-         checkListEmpty();
+         checkArrayEmpty();
          Item item = (Item) queue[current];
          current++;
          return item;
@@ -78,11 +87,42 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
       }
    }
 
+    private void checkArrayEmpty() {
+        if (queue[first] == null) {
+            throw new NoSuchElementException("I was called to remove first/last on an empty list");
+        }
+    }
+
+    private void adjustArraySize(){
+       if (N == queue.length) {
+           cloneArray(N*2);
+
+       } else if (N == queue.length/4) {
+           cloneArray(N/2);
+       }
+   }
+
+   private void cloneArray(int newSize){
+       Object[] newArr = new Object[newSize];
+       int pointer = 0;
+       for (Object object: queue){
+           if (object != null) newArr[++pointer] = object;
+       }
+   }
+
+    // Check if the item is null
+    private void itemValid(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("I was called with to add/remove null");
+        }
+    }
+
     public static void main(String[] args) {
        StdOut.println("###############RandomizedQueue Tests###############");
 
        StdOut.println("##########My Own Test Cases##########");
 
        StdOut.println("####Test 1####");
+       //TODO!!: main() method must call directly every public constructor and method to verify that they work as prescribed
     }
 }
