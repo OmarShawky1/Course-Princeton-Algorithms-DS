3ea7937e1d0155341b3230d86389dea473c12032
Omar Shawky <ipowerfuliii@gmail.com>
2022-08-06 23:37:34 +0200

    Trying to take input using standard input stream

--- Week 2 - Queues/src/Permutation.java
+++ Week 2 - Queues/src/Permutation.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
@@ -6,14 +7,21 @@ public class Permutation {
         //Check that there is only 2 arguments
         if (args.length <= 1) {
             StdOut.println("Insufficient Arguments");
+            for (String arg: args){
+                StdOut.println(arg);
+            }
             return;
         }
 
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
 
-        for (int i = 1; i< args.length; i++){
+        /*for (int i = 1; i< args.length; i++){
          randomizedQueue.enqueue(args[i]);
+        }*/
+
+        while(!StdIn.isEmpty()){
+            randomizedQueue.enqueue(StdIn.readString());
         }
 
         for (int i = 0; i < k; i++){
