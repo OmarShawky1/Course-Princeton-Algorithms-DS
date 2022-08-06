8d9ef907a4c1cbeb787c461e94476ef1b77c2d5a
Omar Shawky <ipowerfuliii@gmail.com>
2022-08-06 14:16:41 +0200

    Testing, Enqueue failing

--- Week 2 - Queues/src/RandomizedQueue.java
+++ Week 2 - Queues/src/RandomizedQueue.java
@@ -8,19 +8,19 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     //Global Variables
 
     //Array object used for storing the enqueued & dequeued items
-    private Object[] queue;
+    private Item[] queue;
     private int numOfItems; //Number of items in an array
     private int lastIndex; //last non-null item in array queue
-    private int first; //first non/null item in array queue
+
+    private final int FIRST = 0; //first non/null item in array queue, always 0
 
 
     // construct an empty randomized queue
     public RandomizedQueue(Item... items) {
-        int itemsLength = items.length;
-        queue = new Object[itemsLength];
-        numOfItems = 0;
+        int itemsLength = items.length == 0 ? 1 : items.length;
+        queue = (Item[]) new Object[itemsLength];
+        numOfItems = itemsLength;
         lastIndex = 0;
-        first = 0;
     }
 
     // is the randomized queue empty?
@@ -30,42 +30,45 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // return the number of items on the randomized queue
     public int size() {
-        return numOfItems + 1;
+        return numOfItems;
     }
 
     // add the item
     public void enqueue(Item item) {
         itemValid(item);
+
+        //check if there is available remaining space before enqueuing
+//        StdOut.println("randomizedQueue.queue.length: " + queue.length); //Todo remove it
+//        StdOut.println("randomizedQueue.numOfItems: " + numOfItems); //Todo remove it
+//        if (queue.length - numOfItems <= 1) {
+//            resize();
+//            queue[++lastIndex] = item;
+//            numOfItems++;
+//            resize();
+//        } else {
+//            queue[++lastIndex] = item;
+//            numOfItems++;
+//            resize();
+//        }
         queue[++lastIndex] = item;
         numOfItems++;
-        adjustArraySize();
+        resize();
     }
 
     // remove and return a random item
     public Item dequeue() {
-        checkArrayEmpty();
-        Item item;
-        int randomNumber;
-        do {
-            randomNumber = first + StdRandom.uniform(numOfItems) + 1;
-            item = (Item) queue[randomNumber];
-        } while (item == null);
+        Object[] node = this.returnRandomNode();
+        int randomNumber = (int) node[0];
+        Item item = (Item) node[1];
         queue[randomNumber] = null;
         numOfItems--;
-        adjustArraySize();
+        resize();
         return item;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-        checkArrayEmpty();
-        Item item;
-        int randomNumber;
-        do {
-            randomNumber = first + StdRandom.uniform(numOfItems) + 1;
-            item = (Item) queue[randomNumber];
-        } while (item == null);
-        return item;
+        return (Item) this.returnRandomNode()[1];
     }
 
     // return an independent iterator over items in random order
@@ -75,48 +78,66 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     private class ListIterator implements Iterator<Item> {
 
-      private int current = first;
+        private int current = FIRST;
 
-      public boolean hasNext() {
-         return queue[current] != null;
-      }
+        public boolean hasNext() {
+            return queue[current] != null;
+        }
 
-      public Item next() {
-         checkArrayEmpty();
-         Item item = (Item) queue[current];
-         current++;
-         return item;
-      }
+        public Item next() {
+            checkArrayEmpty();
+            Item item = (Item) queue[current];
+            current++;
+            return item;
+        }
 
-      public void remove() {
-         throw new UnsupportedOperationException("remove() should not be called");
-      }
-   }
-
-    private void checkArrayEmpty() {
-        if (queue[first] == null) {
-            throw new NoSuchElementException("I was called to remove first/last on an empty list");
+        public void remove() {
+            throw new UnsupportedOperationException("remove() should not be called");
         }
     }
 
-    private void adjustArraySize() {
-       if (numOfItems == queue.length) {
-           cloneArray(numOfItems * 2);
+    //Returns encapsulated node in an object array where first item is index and second is value
+    private Object[] returnRandomNode() {
+        checkArrayEmpty();
+        Item item;
+        int randomNumber;
+        do {
+            randomNumber = this.genRandIndexNumber();
+            item = (Item) queue[randomNumber];
+        } while (item == null);
 
-       } else if (numOfItems == queue.length / 4) {
-           cloneArray(numOfItems / 2);
-       }
-   }
+        return new Object[] {randomNumber, item};
+    }
 
-   private void cloneArray(int newSize) {
-       Object[] newArr = new Object[newSize];
-       int pointer = 0;
-       for (Object object : queue) {
-           if (object != null) newArr[++pointer] = object;
-       }
-       queue = newArr;
-       lastIndex = pointer;
-   }
+    private int genRandIndexNumber() {
+        return StdRandom.uniform(numOfItems);
+    }
+
+    private void checkArrayEmpty() {
+        for (Item item : queue) {
+            if (item != null) return;
+        }
+        throw new NoSuchElementException("I was called to remove first/last on an empty list");
+    }
+
+    private void resize() {
+        if (numOfItems == queue.length) {
+            cloneToArrayOfSize(numOfItems * 2);
+
+        } else if (numOfItems == queue.length / 4) {
+            cloneToArrayOfSize(numOfItems / 2);
+        }
+    }
+
+    private void cloneToArrayOfSize(int newSize) {
+        Item[] newArr = (Item[]) new Object[newSize];
+        int pointer = 0;
+        for (Item item : queue) {
+            if (item != null) newArr[++pointer] = item;
+        }
+        queue = newArr;
+        lastIndex = pointer;
+    }
 
     // Check if the item is null
     private void itemValid(Item item) {
@@ -125,12 +146,51 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
     }
 
+    //TODO: to be removed, this is just for testing purposes to ensure enqueuing is correct
+    private String print() {
+        String temp = "";
+        for (Item item : queue) {
+            temp = temp + item + ",";
+        }
+        return "[" + temp + "]";
+    }
+
     public static void main(String[] args) {
-       StdOut.println("###############RandomizedQueue Tests###############");
+        //TODO!!: main() method must call directly every public constructor and method to verify that they work as prescribed
+        StdOut.println("###############RandomizedQueue Tests###############");
 
-       StdOut.println("##########My Own Test Cases##########");
+        StdOut.println("##########My Own Test Cases##########");
 
-       StdOut.println("####Test 1####");
-       //TODO!!: main() method must call directly every public constructor and method to verify that they work as prescribed
+        StdOut.println("####Test 1####");
+        StdOut.println("Initialize an empty RandomizedQueue");
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        StdOut.println("Constructor & hasNext: " +  randomizedQueue);
+        StdOut.println("list.size() is zero: " + (randomizedQueue.size() == 0));
+        StdOut.println("list.isEmpty() is true: " + randomizedQueue.isEmpty());
+
+        StdOut.println();
+
+        StdOut.println("Start of Error Cases");
+        StdOut.println();
+
+        StdOut.print("list.removeFirst() is error: ");
+        try {
+            StdOut.println(randomizedQueue.dequeue());
+        } catch (NoSuchElementException e) {
+            StdOut.println(e);
+        }
+
+        StdOut.println();
+        StdOut.println("End of Error Cases");
+
+        StdOut.println();
+
+        StdOut.println("\"Enqueue()\" Test Cases");
+        for (int i = 1; i <= 10; i++) {
+            StdOut.println("This is iteration: " + i + " and i will append " + Integer.toString(i)); //Todo remove it
+            randomizedQueue.enqueue(Integer.toString(i));
+        }
+        StdOut.println("list has only [1,2,3,4,5,6,7,8,9,10]: " + randomizedQueue.print());
+        StdOut.println();
     }
 }
