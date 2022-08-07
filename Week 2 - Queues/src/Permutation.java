ba0c03c1ef9cd339b655d95823f80dc8a0f4a0c8
Omar Shawky <ipowerfuliii@gmail.com>
2022-08-07 08:09:12 +0200

    Fixed the stream input bug

--- Week 2 - Queues/src/Permutation.java
+++ Week 2 - Queues/src/Permutation.java
@@ -5,26 +5,24 @@ public class Permutation {
 
     public static void main(String[] args) {
         //Check that there is only 2 arguments
-        if (args.length <= 1) {
+        if (args.length == 0) {
             StdOut.println("Insufficient Arguments");
-            for (String arg: args){
-                StdOut.println(arg);
-            }
             return;
         }
 
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
 
+        // Consume input fed to the environmental variables instead of stream
         /*for (int i = 1; i< args.length; i++){
          randomizedQueue.enqueue(args[i]);
         }*/
 
-        while(!StdIn.isEmpty()){
+        while (!StdIn.isEmpty()) {
             randomizedQueue.enqueue(StdIn.readString());
         }
 
-        for (int i = 0; i < k; i++){
+        for (int i = 0; i < k; i++) {
             StdOut.println(randomizedQueue.dequeue());
         }
     }
