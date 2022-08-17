4cc81f21f56687319c0006ae0493710607821cae
Omar Shawky <ipowerfuliii@gmail.com>
2022-08-17 15:35:44 +0200

    Finished testing CompareTo() & SlopeTo() for all possible cases of 5 points; Will start testing Comparator class

--- Week 3 - Collinear Points/src/Point.java
+++ Week 3 - Collinear Points/src/Point.java
@@ -9,6 +9,8 @@
  ******************************************************************************/
 
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
@@ -19,8 +21,8 @@ public class Point implements Comparable<Point> {
     /**
      * Initializes a new point.
      *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
         /* DO NOT MODIFY */
@@ -55,7 +57,7 @@ public class Point implements Comparable<Point> {
      * Double.POSITIVE_INFINITY if the line segment is vertical;
      * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
@@ -69,7 +71,7 @@ public class Point implements Comparable<Point> {
             return Double.POSITIVE_INFINITY;
         }
 
-        return (double) deltaY/deltaX;
+        return (double) deltaY / deltaX;
     }
 
     /**
@@ -77,16 +79,16 @@ public class Point implements Comparable<Point> {
      * Formally, the invoking point (x0, y0) is less than the argument point
      * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(Point that) {
         // P0 is less than P1, return -1
-        if (y <= that.y && x < that.x)  {
+        if (y <= that.y && x < that.x) {
             return -1;
         }
 
@@ -108,9 +110,8 @@ public class Point implements Comparable<Point> {
         return new BySlope();
     }
 
-    private static class BySlope implements Comparator<Point>{
+    private static class BySlope implements Comparator<Point> {
         public int compare(Point p0, Point p1) {
-//            return p0.compareTo(p1); //TODO: remove it
             return (int) p0.slopeTo(p1);
         }
     }
@@ -132,6 +133,94 @@ public class Point implements Comparable<Point> {
      * Unit tests the Point data type.
      */
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
+
+        StdOut.println("###############RandomizedQueue Tests###############");
+
+        StdOut.println("##########My Own Test Cases##########");
+
+        StdOut.println("####Test 1####");
+        // Initializing 5 points: (0,0), (0,1), (1,0), (1,1), (2,2)
+        Point p00 = new Point(0, 0);
+        Point p01 = new Point(0, 1);
+        Point p10 = new Point(1, 0);
+        Point p11 = new Point(1, 1);
+        Point p22 = new Point(2, 2);
+
+        // Testing CompareTo & SlopeTo
+        // Same point
+        assert (p00.compareTo(p00) == 0) :
+                "p00 = itself but p00.compareTo(p00): " + p00.compareTo(p00);
+
+        assert (p00.slopeTo(p00) == Double.NEGATIVE_INFINITY) :
+                "ΔP = -\\infty but p00.slopeTo(p00): " + p00.slopeTo(p00);
+
+        // Horizontal points/lines (00 --> 10) && (01 --> 11)
+        assert (p00.compareTo(p10) == -1) :
+                "p00 < p10 but p00.compareTo(p10): " + p00.compareTo(p10);
+
+        assert (p01.compareTo(p11) == -1) :
+                "p01 = p11 but p01.compareTo(p11): " + p01.compareTo(p11);
+
+        assert (p00.slopeTo(p10) == 0) :
+                "ΔP = 0 but p00.slopeTo(p10): " + p00.slopeTo(p10);
+
+        assert (p01.slopeTo(p11) == 0) :
+                "ΔP = 0 but p01.slopeTo(p11): " + p01.slopeTo(p11);
+
+
+        // Vertical points/lines (00 --> 01) && (10 --> 11)
+        assert (p00.compareTo(p01) == 0) :
+                "p00 = p01 but p00.compareTo(p01): " + p00.compareTo(p01);
+
+        assert (p10.compareTo(p11) == 0) :
+                "p10 = p11 but p01.compareTo(p11): " + p10.compareTo(p11);
+
+        assert (p00.slopeTo(p01) == Double.POSITIVE_INFINITY) :
+                "ΔP = 0 but p00.slopeTo(p01): " + p00.slopeTo(p01);
+
+        assert (p10.slopeTo(p11) == Double.POSITIVE_INFINITY) :
+                "ΔP = 0 but p10.slopeTo(p11): " + p10.slopeTo(p11);
+
+
+        // Increasing slope (00 --> 11) && (00 --> 22) && (10 --> 22) && (01 --> 22) && (11 --> 22)
+        assert (p00.compareTo(p11) == -1) :
+                "p00 < p11 but p00.compareTo(p11): " + p00.compareTo(p11);
+
+        assert (p00.compareTo(p22) == -1) :
+                "p00 < p22 but p00.compareTo(p22): " + p00.compareTo(p22);
+
+        assert (p10.compareTo(p22) == -1) :
+                "p10 < p22 but p10.compareTo(p22): " + p10.compareTo(p22);
+
+        assert (p01.compareTo(p22) == -1) :
+                "p01 < p22 but p01.compareTo(p22): " + p01.compareTo(p22);
+
+        assert (p11.compareTo(p22) == -1) :
+                "p11 < p22 but p11.compareTo(p22): " + p11.compareTo(p22);
+
+        assert (p00.slopeTo(p11) == 1) :
+                "ΔP = 1 but p00.slopeTo(p11): " + p00.slopeTo(p11);
+
+        assert (p00.slopeTo(p22) == 1) :
+                "ΔP = 1 but p00.slopeTo(p22): " + p00.slopeTo(p22);
+
+        assert (p10.slopeTo(p22) == 2) :
+                "ΔP = 2 but p10.slopeTo(p22): " + p10.slopeTo(p22);
+
+        assert (p01.slopeTo(p22) == 0.5) :
+                "ΔP = 0.5 but p01.slopeTo(p22): " + p01.slopeTo(p22);
+
+        assert (p11.slopeTo(p22) == 1) :
+                "ΔP = 1 but p11.slopeTo(p22): " + p11.slopeTo(p22);
+
+        // Decreasing slope (01 --> 10)
+        assert (p01.compareTo(p10) == 0) :
+                "p01 = p10 but p01.compareTo(p10): " + p01.compareTo(p10);
+
+        assert (p01.slopeTo(p10) == -1) :
+                "ΔP = 1 but p01.slopeTo(p10): " + p01.slopeTo(p10);
+
+        // Testing Comparator Class (SlopeOrder)
+        // TODO!!
     }
 }
