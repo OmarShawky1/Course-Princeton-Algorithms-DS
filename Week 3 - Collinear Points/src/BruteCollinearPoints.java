8788239d3caed12fc7b8e353f582ce938e10aa3a
Omar Shawky <ipowerfuliii@gmail.com>
2022-08-19 10:23:54 +0200

    Removed the old algo implementation of mergesort, finished the third exception check, Will continue the rest of TODOs

--- Week 3 - Collinear Points/src/BruteCollinearPoints.java
+++ Week 3 - Collinear Points/src/BruteCollinearPoints.java
@@ -13,27 +13,36 @@ public class BruteCollinearPoints {
         }
 
         // Return false if a point is null
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null) return false;
+        for (Point p: points) {
+            if (p == null) return false;
         }
 
-        // Return false if a point is repeated.
-        // Decision List:
-        /*
+        // Return false if a point is repeated (Algo: Insertion sort)
+        for (int i=0; i<points.length; i++){
+            Point tempPoint = points[i];
+            for (int j=i+1; j< points.length; j++){
+                if (tempPoint == points[j]) return false;
+            }
+        }
+
+        /* Decision List
          1. First sort them stably via Top-bottom Merge; This will cost ~NlgN (Better than N^2/2
          in selection sort)
             a. That is correct but then you will need to visit the entire array again which costs N
+         2. He does not want me to implement mergesort as per colleagues
         */
+        /* Code Implementation of old validation
         // First, Sort (mergesort)
         // TODO!!
         sort(points, new Point[points.length], 0, points.length - 1);
 
         // Second search in merged result
         // TODO!!
-
+         */
         return true;
     }
 
+    /*
     private static void sort(Point[] points, Point[] aux, int lo, int hi) {
         if (lo >= hi) return;
         int mid = (hi - lo) / 2;
@@ -47,12 +56,12 @@ public class BruteCollinearPoints {
         assert isSorted(points, lo, hi / 2);
         assert isSorted(points, hi / 2 + 1, hi);
 
-        for (int i = 0; i < points.length; i++) aux[i] = points[i]; // Copying Array
+        for (int k = lo; k < hi; k++) aux[k] = points[k]; // Copying Array
 
         // int i, j = 0; // Might cause checkstyle error, to be checked later
         int i = 0; // Pointer to the right half of the array
         int j = 0; // Pointer to the left half of the array
-        for (int k = 0; k < points.length; k++) { // Merging two halves of aux into points array
+        for (int k = 0; k < hi; k++) { // Merging two halves of aux into points array
             if (i >= mid) points[k] = aux[j++]; // Append remaining right half as left is empty
             else if (j >= hi) points[k] = aux[i++]; // Append remaining left half as right is empty
             else if (less(aux[i], aux[j])) points[k] = aux[i++];
@@ -63,14 +72,15 @@ public class BruteCollinearPoints {
     }
 
     private static boolean less(Point aux, Point aux1) {
-        // TODO
+        // !!
         return true;
     }
 
     private static boolean isSorted(Point[] points, int lo, int hi) {
-        // TODO
+        // !!
         return true;
     }
+     */
 
     // the number of line segments
     public int numberOfSegments() {
