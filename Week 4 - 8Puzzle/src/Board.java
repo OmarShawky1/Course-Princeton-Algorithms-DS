ba7a0e001647124bce7df8aa794dd1e43b3318e2
Omar Shawky <ipowerfuliii@gmail.com>
2022-08-29 10:31:26 +0200

    Created all required classes

---
+++ Week 4 - 8Puzzle/src/Board.java
@@ -0,0 +1,44 @@
+public class Board {
+    // create a board from an n-by-n array of tiles,
+// where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+
+    }
+    // string representation of this board
+    public String toString() {
+
+    }
+    // board dimension n
+    public int dimension() {
+
+    }
+    // number of tiles out of place
+    public int hamming() {
+
+    }
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+
+    }
+    // is this board the goal board?
+    public boolean isGoal() {
+
+    }
+    // does this board equal y?
+    public boolean equals(Object y) {
+
+    }
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+
+    }
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+
+    }
+
+    // unit testing (not graded)
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Week 4 - 8Puzzle/src/Solver.java
@@ -0,0 +1,27 @@
+public class Solver {
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+    }
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        // TODO
+        return false;
+    }
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        // TODO
+        return 0;
+    }
+    /*
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+
+    }
+     */
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+    }
+}
