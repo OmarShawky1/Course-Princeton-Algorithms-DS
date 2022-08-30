7e7f5eaad6a89e3fafde0836d0079f66a5eb505f
Omar Shawky <ipowerfuliii@gmail.com>
2022-08-30 18:13:29 +0200

    Board: Enhanced Manhattan logic by removing unecessary cheks for rows and cols

--- Week 4 - 8Puzzle/src/Board.java
+++ Week 4 - 8Puzzle/src/Board.java
@@ -56,45 +56,22 @@ public class Board {
         int numOfOut = 0;
         for (int i = 0; i < tileLength; i++) {
             for (int j = 0; j < tileLength; j++) {
-                int j1 = j + 1;
                 StdOut.println("i: " + i + " & j: " + j); // TODO Remove line
                 int tempTile = tiles[i][j];
-                if (tiles[i][j] != BLANK_TILE && tempTile != (i * tileLength + j1)) { // TODO: check if second part of the check is unnecessary after the function works
-                    StdOut.println("tiles[" + i + "][" + j + "]: " + tiles[i][j] + ", (i * tileLength + j1): " + (i * tileLength + j1)); // TODO Remove line
+                if (tiles[i][j] != BLANK_TILE && tempTile != (i * tileLength + (j + 1))) {
+                    StdOut.println("tiles[" + i + "][" + j + "]: " + tiles[i][j] + ", (i * " + "tileLength + (j + 1)): " + (i * tileLength + (j + 1))); // TODO Remove line
 
                     // check if tile is in correct row
-                    int currentRow = tempTile % tileLength == 0 ?
-                            ((tempTile - 1) / tileLength) : (tempTile / tileLength);
-//                    int currentRow = (tempTile -1) % tileLength;
-                    int deltaRow = i - currentRow;
+                    int goalRow = (tempTile - 1) / tileLength;
+                    int deltaRow = i - goalRow;
                     deltaRow = deltaRow < 0 ? deltaRow * -1 : deltaRow; // Getting absolute
                     numOfOut += deltaRow;
 
                     // check if tile is in correct col
-                    int currentCol = tempTile % tileLength == 0 ? tileLength : tempTile % tileLength;
-                    int deltaCol = j1 - currentCol;
+                    int goalCol = (tempTile - 1) % tileLength;
+                    int deltaCol = j - goalCol;
                     deltaCol = deltaCol < 0 ? deltaCol * -1 : deltaCol; // Getting absolute
                     numOfOut += deltaCol;
-
-                    /*
-                    // Old erroring code; Left in case needed
-                    // check if tile is in correct row
-                    // Obtain the correct current column for the tile in case division is 0 like
-                    // with 5 in case of tileLength is 3, correct Row is 2 but division gives 0
-                    int currentRow = tempTile / tileLength;
-                    int deltaRow = i - currentRow;
-                    deltaRow = deltaRow < 0 ? deltaRow * -1 : deltaRow; // Getting absolute
-                    numOfOut += deltaRow;
-
-                    // check if tile is in correct column
-                    // Obtain the correct current column for the tile in case remainder is 0 like
-                    // with 6 in case of tileLength is 3, correct column is 3 but remainder gives 0
-                    int curreCol = (tempTile % tileLength) == 0 ? (tempTile / tileLength) :
-                            (tempTile % tileLength);
-                    int deltaCol = j1 - curreCol;
-                    deltaCol = deltaCol < 0 ? deltaCol * -1 : deltaCol; // Getting absolute
-                    numOfOut += deltaCol;
-                    */
                     StdOut.println("numOfOut: " + numOfOut + ", deltaRow:" + deltaRow + ", deltaCol" + ": " + deltaCol); // TODO Remove line
                 }
             }
@@ -153,38 +130,46 @@ public class Board {
         Board board = new Board(exampleTiles);
         assert (board.dimension() == 3) : "Dimensions didn't return 3 but instead" + board.dimension();
         StdOut.println("Printing board test: \n" + board);
+        StdOut.println();
 
 
         // Testing compare
         // When equal
         Board board1 = new Board(exampleTiles);
         assert board.equals(board1) : "Boards should be equal but they are not; board: " + board + "\n and board1: " + board1;
+        StdOut.println();
 
         // When different length
         Board board2 = new Board(new int[][]{{1, 2}, {3, 4}, {5, 6}});
         assert !board.equals(board2) : "Boards should not be equal but they are; board: " + board + "\n and board2: " + board2;
+        StdOut.println();
 
 
         // When tiles differ
         Board board3 = new Board(new int[][]{{5, 0, 3}, {4, 2, 5}, {7, 8, 6}});
         assert !board.equals(board3) : "Boards should not be equal but they are; board: " + board + "\n and board3: " + board3;
+        StdOut.println();
 
 
         // Testing Hamming
         assert (board.hamming() == 3) : "hamming() didn't return 3 but instead: " + board.hamming();
+        StdOut.println();
 
         int[][] organizedInt = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
         Board board4 = new Board(organizedInt);
         assert (board4.hamming() == 0) : "hamming() didn't return 0 but instead: " + board4.hamming();
 
+        StdOut.println();
         Board board5 = new Board(new int[][]{{8, 1, 3}, {4, 0, 2}, {7, 6, 5}});
         assert (board5.hamming() == 5) : "hamming() didn't return 5 but instead: " + board5.hamming();
 
         // Testing Manhattan
         assert board.manhattan() == 3 : "manhattan() didn't return 3 but instead: " + board.manhattan();
+        StdOut.println();
 
         assert (board5.manhattan() == 10) : "manhattan() didn't return 10 but instead: " + board5.manhattan();
 
+        StdOut.println();
         assert (board4.manhattan() == 0) : "manhattan() didn't return 0 but instead: " + board4.manhattan();
 
 
