59763daeaf76f7ba7f4ba763ac39d6120110f181
Omar Shawky <ipowerfuliii@gmail.com>
2022-10-12 11:36:12 +0200

    PointSET: Finished Testing; Corrected bugs

--- Week 5 - KD Trees/src/PointSET.java
+++ Week 5 - KD Trees/src/PointSET.java
@@ -52,8 +52,8 @@ public class PointSET {
         LinkedList<Point2D> list = new LinkedList<>();
 
         for (Point2D currentP : rbt) {
-            if ((currentP.x() >= rect.xmin() || currentP.x() <= rect.xmax() &&
-                    (currentP.y() >= rect.ymin() || currentP.y() <= rect.ymax()))) {
+            if ((currentP.x() >= rect.xmin() && currentP.x() <= rect.xmax()) &&
+                    (currentP.y() >= rect.ymin() && currentP.y() <= rect.ymax())) {
                 list.add(currentP);
             }
         }
@@ -65,7 +65,7 @@ public class PointSET {
         if (p == null) throw new IllegalArgumentException("can't search nearest for null");
 
         Iterator<Point2D> rbtIterator = rbt.iterator();
-        if (rbtIterator.hasNext()) {
+        if (!rbtIterator.hasNext()) {
             return null;
         }
 
@@ -88,7 +88,48 @@ public class PointSET {
         StdOut.println("##########My Own Test Cases##########");
 
         StdOut.println("####Test 1####");
+        int numberOfTests = 0;
 
-        
+        // Testing insert
+        PointSET pointSET = new PointSET();
+        pointSET.insert(new Point2D(0,0));
+        StdOut.println("Test: " + ++numberOfTests + " passed");
+
+        try {
+            pointSET.insert(null);
+        } catch (IllegalArgumentException e) {
+            StdOut.println(e);
+        }
+        StdOut.println("Test: " + ++numberOfTests + " passed");
+
+
+        // Testing Contains
+        if (!pointSET.contains(new Point2D(0,0))) throw new RuntimeException("Should've contained origin");
+        StdOut.println("Test: " + ++numberOfTests + " passed");
+
+        if (pointSET.contains(new Point2D(0,1))) throw new RuntimeException("Shouldn't contain (0,1)");
+        StdOut.println("Test: " + ++numberOfTests + " passed");
+
+        // Testing range
+        Point2D origin = new Point2D(0,0);
+        Point2D P01 = new Point2D(0,1);
+        Point2D P10 = new Point2D(1,0);
+        Point2D P11 = new Point2D(1,1);
+        Point2D P21 = new Point2D(2,1);
+        RectHV rectHV = new RectHV(0,0,1,1);
+
+        pointSET = new PointSET();
+        pointSET.insert(origin);
+        pointSET.insert(P01);
+        pointSET.insert(P10);
+        pointSET.insert(P11);
+        pointSET.insert(P21);
+
+        if (((LinkedList) pointSET.range(rectHV)).size() != 4) throw new RuntimeException("Should've gotten exactly 4 points but instead: " + ((LinkedList) pointSET.range(rectHV)).size());
+        StdOut.println("Test: " + ++numberOfTests + " passed");
+
+        // Testing nearest
+        if (pointSET.nearest(new Point2D(2,2)).compareTo(P21) != 0) throw new RuntimeException("Nearest should've been P21 but instead it is " + pointSET.nearest(P21));
+        StdOut.println("Test: " + ++numberOfTests + " passed");
     }
 }
