66ca497d506b5a15d0f1efd23ce27b994acd7f38
Omar Shawky <ipowerfuliii@gmail.com>
2022-11-13 13:19:46 +0200

    Initialized Week 7

---
+++ Week 7 - Seam Carving/src/SeamCarver.java
@@ -0,0 +1,52 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+
+    }
+
+    // current picture
+    public Picture picture() {
+
+    }
+
+    // width of current picture
+    public int width() {
+
+    }
+
+    // height of current picture
+    public int height() {
+
+    }
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+
+    // unit testing (optional)
+    public static void main(String[] args)
+}
