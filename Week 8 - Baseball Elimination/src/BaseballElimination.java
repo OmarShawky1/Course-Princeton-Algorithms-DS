151390883f23ff1430a3d47241507ba90d671dd2
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-01 11:50:02 +0200

    Implemented Baseball elimination; Grade 100/100

--- Week 8 - Baseball Elimination/src/BaseballElimination.java
+++ Week 8 - Baseball Elimination/src/BaseballElimination.java
@@ -1,16 +1,23 @@
+import edu.princeton.cs.algs4.FordFulkerson;
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.SET;
 
-import java.util.LinkedList;
+import java.util.HashMap;
 
 public class BaseballElimination {
 
     // Global Variables
+    private static final int SOURCE_INDEX = 0;
+    private static final int SINK_INDEX = 1;
     private final int[][] against; // games left between team i and team j
-    private final int[] wins, losses;
-    private final int[] remaining; // games left for team x in general
+    private final int[] wins, losses, remaining;
     private final String[] teams;
     private final int numberOfTeams;
+    private final HashMap<String, Integer> teamsHM;
+    private final HashMap<String, SET<String>> eliminatingTeamsHM;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
@@ -24,10 +31,14 @@ public class BaseballElimination {
         losses = new int[numberOfTeams];
         remaining = new int[numberOfTeams];
         teams = new String[numberOfTeams];
+        teamsHM = new HashMap<>();
+        eliminatingTeamsHM = new HashMap<>();
 
         // register each teams name, wins, loses, remaining
         for (int i = 0; !in.isEmpty(); i++) {
-            teams[i] = in.readString();
+            String teamName = in.readString();
+            teamsHM.put(teamName, i);
+            teams[i] = teamName;
             wins[i] = in.readInt();
             losses[i] = in.readInt();
             remaining[i] = in.readInt();
@@ -35,10 +46,7 @@ public class BaseballElimination {
             // register games left for team i against every other team
             for (int j = 0; j < numberOfTeams; j++) against[i][j] = in.readInt();
         }
-        
         in.close();
-
-        //TODO: Construct Flow Network graph
     }
 
     // number of teams
@@ -46,48 +54,121 @@ public class BaseballElimination {
         return numberOfTeams;
     }
 
-
     // all teams
     public Iterable<String> teams() {
-        return new LinkedList<>(); //TODO
+        return teamsHM.keySet();
     }
-
 
     // number of wins for given team
     public int wins(String team) {
-        return wins[indexOf(team)];
+        return wins[teamIndex(team)];
     }
 
     // number of losses for given team
     public int losses(String team) {
-        return losses[indexOf(team)];
+        return losses[teamIndex(team)];
     }
 
     // number of remaining games for given team
     public int remaining(String team) {
-        return remaining[indexOf(team)];
+        return remaining[teamIndex(team)];
     }
 
     // number of remaining games between team1 and team2
     public int against(String team1, String team2) {
-        return against[indexOf(team1)][indexOf(team2)];
-    }
-
-    private int indexOf(String team1) {
-        for (int i = 0; i < numberOfTeams; i++) {
-            if (team1.equals(teams[i])) return i;
-        }
-        return -1; // team not found
+        return against[teamIndex(team1)][teamIndex(team2)];
     }
 
     // is given team eliminated?
     public boolean isEliminated(String team) {
-        return false; //TODO
+        return eliminationSet(team).iterator().hasNext();
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
-        return new LinkedList<>(); //TODO
+        return isEliminated(team) ? eliminationSet(team) : null;
+    }
+
+    private Iterable<String> eliminationSet(String team) {
+        if (teamsHM.get(team) == null) throw new IllegalArgumentException();
+
+        if (eliminatingTeamsHM.get(team) == null && !isAlreadyEliminated(team)) evaluateEliminatingTeams(team);
+
+        return eliminatingTeamsHM.get(team);
+    }
+
+    // checks for elimination without drawing flow network; Just from the current scores
+    private boolean isAlreadyEliminated(String team) {
+        SET<String> eliminatingTeams = new SET<>();
+        int maxWins = wins[teamIndex(team)] + remaining[teamIndex(team)];
+
+        for (String t : teamsHM.keySet()) if (wins[teamsHM.get(t)] > maxWins) eliminatingTeams.add(t);
+
+        eliminatingTeamsHM.put(team, eliminatingTeams);
+        return !eliminatingTeams.isEmpty();
+    }
+
+    // calculates -if possible- subset of R set that eliminates team *team*
+    // check files in Miscellaneous folder for further explanation
+    private void evaluateEliminatingTeams(String team) {
+        int TI = teamIndex(team); // teams' index
+        int opponentTeams = teams.length - 1;
+
+        // nCr (n choose r) where r=2 which is # of teams per game
+        // int opponentsGames = factorial(opponentTeams)/(factorial(opponentTeams - 2) * 2); //// un-comment "factorial"
+        // alternative way, n teams play, each team plays with (n-1) teams (to not count itself), don't count game twice
+        int opponentsGames = opponentTeams * (opponentTeams - 1) / 2;
+
+        int vertices = 2 + opponentsGames + opponentTeams; // 2 is the count of source and sink
+
+        // Create & Connect the flow network
+        FlowNetwork network = new FlowNetwork(vertices);
+
+        // Check table in Assignment while reading
+        // For each row (i), connect current team to sink
+        int gameIndex = 2 + opponentTeams; // maintain pointer for played games independent of other pointers
+        double maxWins = wins[TI] + remaining[TI]; // Used to limit any team wins to maxWins
+        for (int i = 0; i < numberOfTeams; i++) {
+            if (i == TI) continue;
+
+            network.addEdge(new FlowEdge(flowNetworkTI(i, TI), SINK_INDEX, maxWins - wins[i])); // T-->sink
+
+            // for each column (j) connect source to game, game to team 1 & 2
+            // Don't add game_ij unless j>i to avoid adding edges from source-->game twice (once from r_ij & other r_ji)
+            for (int j = 0; j < i; j++) {
+                if (j == TI) continue;
+
+                // add edge source-->game_ij with vertex-index "gameIndex" & capacity r_ij
+                network.addEdge(new FlowEdge(SOURCE_INDEX, gameIndex, against[i][j]));
+
+                // add game-->team edge
+                network.addEdge(new FlowEdge(gameIndex, flowNetworkTI(i, TI), Double.POSITIVE_INFINITY)); // team 1
+                network.addEdge(new FlowEdge(gameIndex, flowNetworkTI(j, TI), Double.POSITIVE_INFINITY)); // team 2
+
+                gameIndex++; // next game
+            }
+        }
+
+        // calculate the mincut from the maxflow by detecting the not-full-forward-edge which represents number of games
+        // that cannot be played due to the capacity constraints "maxWins"
+        FordFulkerson maxFlow = new FordFulkerson(network, SOURCE_INDEX, SINK_INDEX);
+        SET<String> eliminatingTeams = new SET<>();
+        for (int i = 0; i < numberOfTeams; i++)
+            if (i != TI && maxFlow.inCut(flowNetworkTI(i, TI))) eliminatingTeams.add(teams[i]);
+
+        eliminatingTeamsHM.put(team, eliminatingTeams);
+    }
+
+    private int teamIndex(String team) {
+        Integer ind = teamsHM.get(team);
+        if (ind == null) throw new IllegalArgumentException();
+        return ind;
+    }
+
+    // maps index of i in team[i] to vertex v in the flow network
+    private int flowNetworkTI(int i, int TI) {
+        // shift right by 2 (source & sink vertices are 0 & 1 respectively) or shift left as studiedTeam isn't included
+        return i < TI ? i + 2 : i + 1;
     }
 
     public static void main(String[] args) {
@@ -104,4 +185,11 @@ public class BaseballElimination {
             }
         }
     }
+    /*
+    private int factorial(int i) {
+        int acc = 1;
+        while (i > 1) acc *= i--;
+        return acc;
+    }
+    */
 }
