495fbc532474505a76df01529b4ce0fa7c62bb4a
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-12 21:39:47 +0200

    Implemented almost everything; Facing a bug while testing

--- Week 9 - Boggle/src/BoggleSolver.java
+++ Week 9 - Boggle/src/BoggleSolver.java
@@ -1,25 +1,95 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.TST;
 
-import java.util.LinkedList;
+import java.util.HashSet;
 
 public class BoggleSolver {
+    private final TST trie = new TST<>();
 
-    // Initializes the data structure using the given array of strings as the dictionary.
-    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-        //TODO
+        if (dictionary == null) throw new IllegalArgumentException();
+
+        for (int i = 0; i < dictionary.length; ++i) trie.put(dictionary[i], i);
     }
 
-    // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        return new LinkedList<>(); //TODO
+        if (board == null) throw new IllegalArgumentException();
+
+        HashSet<String> validWords = new HashSet();
+
+        for (int x = 0; x < board.cols(); ++x) {
+            for (int y = 0; y < board.rows(); ++y) {
+                navAdjTiles(board, new Position(this, x, y), validWords, new HashSet(), "");
+            }
+        }
+
+        return validWords;
     }
 
-    // Returns the score of the given word if it is in the dictionary, zero otherwise.
-    // (You can assume the word contains only the uppercase letters A through Z.)
+    private void navAdjTiles(BoggleBoard board, Position position, HashSet<String> validWords, HashSet<Position> path, String word) {
+        if (position.x >= 0 && position.x < board.cols()) {
+            if (position.y >= 0 && position.y < board.rows()) {
+                //TODO: Not correctly checking if path contains
+                if (!path.contains(position)) {
+                    path.add(position);
+                    String letter = String.valueOf(board.getLetter(position.y, position.x));
+                    if (letter.equals("Q")) letter = letter + "U";
+
+                    word += letter;
+                    if (word.length() > 2 && trie.get(letter) != null && !validWords.contains(word)) {
+                        validWords.add(word);
+                    }
+
+                    HashSet newPath = new HashSet<>(path);
+                    newPath.add(position);
+                    this.navAdjTiles(board, new Position(this, position.x + 1, position.y), validWords, newPath, word);
+                    this.navAdjTiles(board, new Position(this, position.x - 1, position.y), validWords, newPath, word);
+                    this.navAdjTiles(board, new Position(this, position.x, position.y + 1), validWords, newPath, word);
+                    this.navAdjTiles(board, new Position(this, position.x, position.y - 1), validWords, newPath, word);
+                    this.navAdjTiles(board, new Position(this, position.x + 1, position.y + 1), validWords, newPath, word);
+                    this.navAdjTiles(board, new Position(this, position.x + 1, position.y - 1), validWords, newPath, word);
+                    this.navAdjTiles(board, new Position(this, position.x - 1, position.y + 1), validWords, newPath, word);
+                    this.navAdjTiles(board, new Position(this, position.x - 1, position.y - 1), validWords, newPath, word);
+                }
+            }
+        }
+    }
+
     public int scoreOf(String word) {
-        return 0; //TODO
+        if (word == null) throw new IllegalArgumentException();
+
+        if (trie.get(word) != null) {
+            switch (word.length()) {
+                case 0:
+                case 1:
+                case 2:
+                    return 0;
+                case 3:
+                case 4:
+                    return 1;
+                case 5:
+                    return 2;
+                case 6:
+                    return 3;
+                case 7:
+                    return 5;
+                default:
+                    return 11;
+            }
+        }
+        return 0;
+    }
+
+    private class Position {
+        private final BoggleSolver boggleSolver;
+        private final int x, y;
+
+        public Position(BoggleSolver boggleSolver, int x, int y) {
+            this.boggleSolver = boggleSolver;
+            this.x = x;
+            this.y = y;
+        }
     }
 
     public static void main(String[] args) {
