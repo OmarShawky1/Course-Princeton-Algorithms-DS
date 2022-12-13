e8a7f688d240aadcf73b8e7a830bc25108a581e2
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-13 11:04:00 +0200

    Minor checkstyle bug fixes; Grade 86/100

--- Week 9 - Boggle/src/BoggleSolver.java
+++ Week 9 - Boggle/src/BoggleSolver.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 
 public class BoggleSolver {
-    private final TST trie = new TST<>();
+    private final TST<Object> trie = new TST<>();
 
     public BoggleSolver(String[] dictionary) {
         if (dictionary == null) throw new IllegalArgumentException();
@@ -17,11 +17,11 @@ public class BoggleSolver {
     public Iterable<String> getAllValidWords(BoggleBoard board) {
         if (board == null) throw new IllegalArgumentException();
 
-        HashSet<String> validWords = new HashSet();
+        HashSet<String> validWords = new HashSet<>();
 
         for (int x = 0; x < board.cols(); ++x)
             for (int y = 0; y < board.rows(); ++y)
-                navAdjTiles(board, new Position(x, y), validWords, new HashSet(), "");
+                navAdjTiles(board, new Position(x, y), validWords, new HashSet<>(), "");
 
         return validWords;
     }
@@ -36,11 +36,19 @@ public class BoggleSolver {
             // Backtracking optimization, check if this word exists in the dictionary
             if (!trie.keysWithPrefix(word).iterator().hasNext()) return;
 
+            // Exploit that fact that when you perform a prefix query operation, it is usually almost identical to the
+            // previous prefix query, except that it is one letter longer.
+
+            // Consider a nonrecursive implementation of the prefix query operation.
+
+            // Precompute the Boggle graph, i.e., the set of cubes adjacent to each cube. But don't necessarily use a
+            // heavyweight Graph object.
+
             // No need to check if the word is already added, "add" already does so.
             if (word.length() > 2 && trie.get(word) != null) validWords.add(word);
 
 
-            HashSet newPath = new HashSet<>(path);
+            HashSet<Position> newPath = new HashSet<>(path);
             newPath.add(p);
             navAdjTiles(board, new Position(p.x + 1, p.y), validWords, newPath, word);
             navAdjTiles(board, new Position(p.x - 1, p.y), validWords, newPath, word);
@@ -56,26 +64,6 @@ public class BoggleSolver {
     public int scoreOf(String word) {
         if (word == null) throw new IllegalArgumentException();
 
-        /*if (trie.get(word) != null) {
-            switch (word.length()) {
-                case 0:
-                case 1:
-                case 2:
-                    return 0;
-                case 3:
-                case 4:
-                    return 1;
-                case 5:
-                    return 2;
-                case 6:
-                    return 3;
-                case 7:
-                    return 5;
-                default:
-                    return 11;
-            }
-        }
-        return 0;*/
         switch (word.length()) {
             case 3:
             case 4:
@@ -120,7 +108,8 @@ public class BoggleSolver {
 
         // Temp test
         // board that provides only two words, ACER & ACERS
-        /* char[][] customChar = {{'S', 'S', 'S', 'S'},
+        /*
+        char[][] customChar = {{'S', 'S', 'S', 'S'},
                 {'A', 'C', 'E', 'R'},
                 {'S', 'S', 'S', 'S'}};
         BoggleBoard board = new BoggleBoard(customChar);
