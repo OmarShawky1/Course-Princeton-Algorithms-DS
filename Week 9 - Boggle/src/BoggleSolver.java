6a091467c20bb4ce4da218915e80e94f6aba215b
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-13 12:05:42 +0200

    Fixed correctness bug; Grade 91/100

--- Week 9 - Boggle/src/BoggleSolver.java
+++ Week 9 - Boggle/src/BoggleSolver.java
@@ -64,19 +64,26 @@ public class BoggleSolver {
     public int scoreOf(String word) {
         if (word == null) throw new IllegalArgumentException();
 
-        switch (word.length()) {
-            case 3:
-            case 4:
-                return 1;
-            case 5:
-                return 2;
-            case 6:
-                return 3;
-            case 7:
-                return 5;
-            default:
-                return 11;
+        if (trie.get(word) != null) {
+            switch (word.length()) {
+                case 0:
+                case 1:
+                case 2:
+                    return 0;
+                case 3:
+                case 4:
+                    return 1;
+                case 5:
+                    return 2;
+                case 6:
+                    return 3;
+                case 7:
+                    return 5;
+                default:
+                    return 11;
+            }
         }
+        return 0;
     }
 
     private static class Position {
