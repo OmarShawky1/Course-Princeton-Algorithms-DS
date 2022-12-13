154157525e5052602fd909fdd37f58a94cdb0f39
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-13 19:00:26 +0200

    Minor enhancement in path detection; Grade 91/100 but slightly faster as per the autograder; will try to implement TST so that I don't need to traverse tree everytime

--- Week 9 - Boggle/src/BoggleSolver.java
+++ Week 9 - Boggle/src/BoggleSolver.java
@@ -1,12 +1,12 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.TST;
 
 import java.util.HashSet;
 import java.util.Objects;
 
 public class BoggleSolver {
-    // private final TST<Integer> trie = new TST<>();
-    private final TerTrie<Integer> trie = new TerTrie<>();
+    private final TST<Integer> trie = new TST<>();
 
     public BoggleSolver(String[] dictionary) {
         if (dictionary == null) throw new IllegalArgumentException();
@@ -21,21 +21,20 @@ public class BoggleSolver {
 
         for (int x = 0; x < board.cols(); ++x)
             for (int y = 0; y < board.rows(); ++y)
-                navAdjTiles(board, new Position(x, y), validWords, new boolean[board.rows()][board.cols()], "", trie);
+                navAdjTiles(board, new Position(x, y), validWords, new boolean[board.rows()][board.cols()], "");
 
         return validWords;
     }
 
     // Navigate all 8 neighboring tiles; right, left, down, up and 4 diagonals respectively
-    private void navAdjTiles(BoggleBoard board, Position p, HashSet<String> validWords,
-                             boolean[][] path, String word, TerTrie<Integer> trie) {
+    private void navAdjTiles(BoggleBoard board, Position p, HashSet<String> validWords, boolean[][] path, String word) {
         if (p.x >= 0 && p.x < board.cols() && p.y >= 0 && p.y < board.rows() && !path[p.y][p.x]) {
             String letter = String.valueOf(board.getLetter(p.y, p.x));
             if (letter.equals("Q")) letter = letter + "U";
             word += letter;
 
             // Backtracking optimization, check if this word exists in the dictionary
-            if (!trie.keysWithPrefix(letter).iterator().hasNext()) return;
+            if (!trie.keysWithPrefix(word).iterator().hasNext()) return;
 
             /*
             * TODO: Enhancement suggestions by FAQs
@@ -49,29 +48,20 @@ public class BoggleSolver {
             */
 
             // No need to check if the word is already added, "add" already does so.
-            // TODO: here is the bug that causes not adding the word; "trie.get(letter) != null"
-            // To recreate the problem, set a break point at "word+=letter" with value "word.equals("AR") && letter.equals("S")"
-            if (word.length() > 2 && trie.get(letter) != null) validWords.add(word);
+            if (word.length() > 2 && trie.get(word) != null) validWords.add(word);
 
-            //TODO: use "Techno Chess, Atomic variation" suggestion to set value true and swap it back after recursion
-            // instead of cloning entire array
 
-            // Create array of booleans that represents visited tiles with initial values to false
-            boolean[][] newPath = new boolean[board.cols()][board.rows()];
-            for (int row = 0; row < board.cols(); row++) newPath[row] = path[row].clone(); // clone old bol[][]
-            newPath[p.y][p.x] = true; // add current tile as a visited tile
 
-            // instantiate a new TerTrie from current node (to escape unnecessary tree traversal)
-            TerTrie<Integer> newTrie = new TerTrie<>(trie.getSubTrie(letter));
-
-            navAdjTiles(board, new Position(p.x + 1, p.y), validWords, newPath, word, newTrie);
-            navAdjTiles(board, new Position(p.x - 1, p.y), validWords, newPath, word, newTrie);
-            navAdjTiles(board, new Position(p.x, p.y + 1), validWords, newPath, word, newTrie);
-            navAdjTiles(board, new Position(p.x, p.y - 1), validWords, newPath, word, newTrie);
-            navAdjTiles(board, new Position(p.x + 1, p.y + 1), validWords, newPath, word, newTrie);
-            navAdjTiles(board, new Position(p.x + 1, p.y - 1), validWords, newPath, word, newTrie);
-            navAdjTiles(board, new Position(p.x - 1, p.y + 1), validWords, newPath, word, newTrie);
-            navAdjTiles(board, new Position(p.x - 1, p.y - 1), validWords, newPath, word, newTrie);
+            path[p.y][p.x] = true;
+            navAdjTiles(board, new Position(p.x + 1, p.y), validWords, path, word);
+            navAdjTiles(board, new Position(p.x - 1, p.y), validWords, path, word);
+            navAdjTiles(board, new Position(p.x, p.y + 1), validWords, path, word);
+            navAdjTiles(board, new Position(p.x, p.y - 1), validWords, path, word);
+            navAdjTiles(board, new Position(p.x + 1, p.y + 1), validWords, path, word);
+            navAdjTiles(board, new Position(p.x + 1, p.y - 1), validWords, path, word);
+            navAdjTiles(board, new Position(p.x - 1, p.y + 1), validWords, path, word);
+            navAdjTiles(board, new Position(p.x - 1, p.y - 1), validWords, path, word);
+            path[p.y][p.x] = false;
         }
     }
 
