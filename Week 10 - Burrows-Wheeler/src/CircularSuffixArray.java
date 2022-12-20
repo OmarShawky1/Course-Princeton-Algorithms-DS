3cd6fb3b7e949e7a9e3bb3068e94f995fce265ee
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-20 20:52:26 +0200

    Discovered issues with current implementation (I was creating arrays for every suffix) and will revert and reimplement

--- Week 10 - Burrows-Wheeler/src/CircularSuffixArray.java
+++ Week 10 - Burrows-Wheeler/src/CircularSuffixArray.java
@@ -4,6 +4,18 @@ public class CircularSuffixArray {
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
+        if (s == null) throw new IllegalArgumentException("s is null");
+
+        char[] c = s.toCharArray(); //TODO: no need for toChar, remove it after finishing
+        char[][] suffixArr = new char[c.length][c.length];
+
+        // store suffixes
+        for (int i = 0; i < c.length; i++) {
+            for (int j = i; j < c.length; j++) suffixArr[i][j-i] = c[j];
+            for (int k = 0; k < i; k++) suffixArr[i][c.length-i+k] = c[k];
+        }
+
+        // TODO: Merge sort
     }
     
     // length of s
@@ -18,6 +30,6 @@ public class CircularSuffixArray {
     
     // unit testing (required)
     public static void main(String[] args) {
-        
+        CircularSuffixArray cir = new CircularSuffixArray("ABRACADABRA!");
     }
 }
