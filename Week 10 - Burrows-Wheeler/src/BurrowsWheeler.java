295a9bc88c3a1b27c2cf706c77904d42040e4840
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-20 22:21:20 +0200

    BurrowsWheeler: Initialized Class

---
+++ Week 10 - Burrows-Wheeler/src/BurrowsWheeler.java
@@ -0,0 +1,20 @@
+public class BurrowsWheeler {
+
+    // apply Burrows-Wheeler transform,
+    // reading from standard input and writing to standard output
+    public static void transform() {
+        //TODO
+    }
+
+    // apply Burrows-Wheeler inverse transform,
+    // reading from standard input and writing to standard output
+    public static void inverseTransform() {
+        //TODO
+    }
+
+    // if args[0] is "-", apply Burrows-Wheeler transform
+    // if args[0] is "+", apply Burrows-Wheeler inverse transform
+    public static void main(String[] args) {
+
+    }
+}
