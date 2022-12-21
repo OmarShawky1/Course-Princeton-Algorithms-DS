6c4a7e56fdb6e576b77e84c9527ca887f83ab1b6
Omar Shawky <ipowerfuliii@gmail.com>
2022-12-21 20:07:16 +0200

    BurrowsWheeler: Corrected a logical bug; made it work for multi-line files

--- Week 10 - Burrows-Wheeler/src/BurrowsWheeler.java
+++ Week 10 - Burrows-Wheeler/src/BurrowsWheeler.java
@@ -6,31 +6,34 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform() {
-        if (BinaryStdIn.isEmpty()) return;
+        // For each line
+        while (!BinaryStdIn.isEmpty()) {
+            // Read input and initialize suffixArray from it
+            String input = BinaryStdIn.readString();
+            CircularSuffixArray cir = new CircularSuffixArray(input);
 
-        // Read input and initialize suffixArray from it
-        String input = BinaryStdIn.readString();
-        CircularSuffixArray cir = new CircularSuffixArray(input);
+            // find "first" & t
+            int l = input.length();
+            char[] t = new char[l];
+            int first = 0;
+            for (int i = 0; i < input.length(); i++) {
+                int temp = cir.index(i);
+                t[i] = input.charAt((temp + l - 1) % l);
+                if (temp == 0) first = i;
+            }
 
-        // find first & t
-        int l = input.length();
-        char[] t = new char[l];
-        int first = 0;
-        for (int i = 0; i < input.length(); i++) {
-            int temp = cir.index(i);
-            t[i] = input.charAt((temp + l - 1) % l);
-            if (temp == 0) first = i;
+            // print "first" then t.
+            BinaryStdOut.write(first);
+            for (int i = 0; i < t.length; i++) BinaryStdOut.write(t[i]);
+
+            BinaryStdIn.close();
+            BinaryStdOut.close();
         }
-
-        // print first then t.
-        BinaryStdOut.write(first);
-        for (int i = 0; i < t.length; i++) BinaryStdOut.write(t[i]);
     }
-
-    // apply Burrows-Wheeler inverse transform,
-    // reading from standard input and writing to standard output
+    
+    // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
     public static void inverseTransform() {
-        //TODO
+
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
